2138376f78cec18383b05d55f75e349ee0bb8bf5
TheSoftwareWizard <roajeanmarco@gmail.com>
2025-04-10 20:51:16 -0400

    added new files

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -13,7 +13,7 @@ return {
 	
 	-- default_prog = {"/bin/zsh", "-l", "-c", "tmux attach || tmux new-session"},
 
-	color_scheme= 'Catppuccin Mocha',
+	color_scheme= 'Everforest',
 
 	-- Font config
 	font = font_with_fallback(font_name),
@@ -68,4 +68,22 @@ return {
 	window_background_opacity = 0.7,
 	window_close_confirmation = "NeverPrompt",
   window_frame = { active_titlebar_bg = "#45475a", font = font_with_fallback(font_name, { bold = true }) },
+
+--   return {
+-- 	colors = {
+-- 	  foreground = "#d0f0c0",          -- Texto verde claro
+-- 	  background = "#1a2f1a",          -- Fondo verde oscuro
+-- 	  cursor_bg = "#73d073",           -- Cursor verde brillante
+-- 	  cursor_border = "#73d073",
+-- 	  ansi = {
+-- 		"#1a2f1a", "#5f9e5f", "#8ac48a", "#a3d9a3",
+-- 		"#4a784a", "#6b9e6b", "#87b787", "#c0e0c0"
+-- 	  },
+-- 	  brights = {
+-- 		"#2d4d2d", "#7fbf7f", "#9fd99f", "#b3e6b3",
+-- 		"#5f8f5f", "#8fbf8f", "#a3d9a3", "#d0f0d0"
+-- 	  },
+-- 	  indexed = { [136] = "#af8700" }, -- Ajustes adicionales
+-- 	},
+--   }
 }
